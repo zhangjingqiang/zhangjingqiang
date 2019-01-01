3dc68bd3949bc15ab485072a775dd613210b9e4a
zhangjingqiang <zhangjingqiang@gmail.com>
2019-01-01 11:41:46 +0900

    Remove Google Maps

--- public/assets/js/main-20150307.js
+++ public/assets/js/main-20150307.js
@@ -32,32 +32,6 @@
 
 
 		/*-----------------------------------/
-		/* GOOGLE MAPS
-		/*----------------------------------*/
-
-		if( $('.map-canvas').length > 0) {
-
-			var geocoder = new google.maps.Geocoder();
-			var address = 'California';
-			var contentString = '<div class="map-detail"><strong>Our Office:</strong><p>' + address + '</p></div>';
-
-			geocoder.geocode({'address': address }, function(results, status) {
-				if(status == google.maps.GeocoderStatus.OK) {
-					var latitude = results[0].geometry.location.lat();
-					var longitude = results[0].geometry.location.lng();
-
-					jQuery('.map-canvas').gmap().bind('init', function(ev, map) {
-						jQuery('.map-canvas').gmap('addMarker', {'position': latitude+','+longitude, 'bounds': true}).click(function() {
-							jQuery('.map-canvas').gmap('openInfoWindow', {'content': contentString}, this);
-						});
-						jQuery('.map-canvas').gmap('option', 'zoom', 8);
-					});
-				}else { alert('Google Maps had some trouble finding the address. Status: ' + status); }
-			});
-		}
-
-
-		/*-----------------------------------/
 		/* AJAX CONTACT FORM
 		/*----------------------------------*/
 
